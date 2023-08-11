fc8e47d56c8fa615176115343e69d76021744f87
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-11 20:52:40 -0300

    Arrumando a função de emprestar livros

--- biblioteca.py
+++ biblioteca.py
@@ -3,24 +3,38 @@
 class Biblioteca():
     def __init__(self, gerente):
         self.gerente = gerente
-
         self.banco_dados_usuarios = []
         self.livro_emprestado = []
-        # Banco de dados da biblioteca
         self._livros_cadastrados = [
-            {'titulo': 'Dom Quixote', 'ano': '1605', 'autor': 'Miguel de Cervantes', 'genero': 'Romance'}, 
-            {'titulo': '1984', 'ano': '1949', 'autor': 'George Orwell', 'genero': 'Distopia'}, 
-            {'titulo': 'A Guerra dos Tronos', 'ano': '1996', 'autor': 'George R.R. Martin', 'genero': 'Fantasia'}, 
-            {'titulo': 'O Pequeno Príncipe', 'ano': '1943', 'autor': 'Antoine de Saint-Exupéry', 'genero': 'Fábula'}, 
-            {'titulo': 'Crime e Castigo', 'ano': '1866', 'autor': 'Fiódor Dostoiévski', 'genero': 'Romance'}, 
-            {'titulo': 'O Hobbit', 'ano': '1937', 'autor': 'J.R.R. Tolkien', 'genero': 'Fantasia'}, 
-            {'titulo': 'Percy Jackson e o Ladrão de Raios', 'ano': '2005', 'autor': 'Rick Riordan', 'genero': 'Fantasia'}
+            {'Codigo do Livro': '0001', 'titulo': 'Dom Quixote', 'ano': '1605', 'autor': 'Miguel de Cervantes', 'genero': 'Romance'},
+            {'Codigo do Livro': '0032', 'titulo': 'A Guerra dos Tronos', 'ano': '1996', 'autor': 'George R.R. Martin', 'genero': 'Fantasia'},
+            {'Codigo do Livro': '0078', 'titulo': 'Orgulho e Preconceito', 'ano': '1813', 'autor': 'Jane Austen', 'genero': 'Romance Clássico'},
+            {'Codigo do Livro': '0234', 'titulo': 'O Senhor dos Anéis: A Sociedade do Anel', 'ano': '1954', 'autor': 'J.R.R. Tolkien', 'genero': 'Fantasia'},
+            {'Codigo do Livro': '0321', 'titulo': 'Cem Anos de Solidão', 'ano': '1967', 'autor': 'Gabriel García Márquez', 'genero': 'Mágico'},
+            {'Codigo do Livro': '0567', 'titulo': 'O Pequeno Príncipe', 'ano': '1943', 'autor': 'Antoine de Saint-Exupéry', 'genero': 'Literatura'},
+            {'Codigo do Livro': '0809', 'titulo': 'A Revolução dos Bichos', 'ano': '1945', 'autor': 'George Orwell', 'genero': 'Ficção'},
+            {'Codigo do Livro': '1098', 'titulo': 'O Alquimista', 'ano': '1988', 'autor': 'Paulo Coelho', 'genero': 'Ficção'},
+            {'Codigo do Livro': '1543', 'titulo': 'O Retrato de Dorian Gray', 'ano': '1890', 'autor': 'Oscar Wilde', 'genero': 'Ficção'},
+            {'Codigo do Livro': '2015', 'titulo': '1984', 'ano': '1949', 'autor': 'George Orwell', 'genero': 'Ficção'},
+            {'Codigo do Livro': '2356', 'titulo': 'Harry Potter e a Pedra Filosofal', 'ano': '1997', 'autor': 'J.K. Rowling', 'genero': 'Fantasia'},
         ]
-        
-        self._estoque = [ 
+#-------------------------------------------------------------------------------------------------------------------------------
+
+        self._estoque = [
+            {'Codigo do Livro': '0001', 'titulo': 'Dom Quixote', 'ano': '1605', 'autor': 'Miguel de Cervantes', 'genero': 'Romance'},
+            {'Codigo do Livro': '0032', 'titulo': 'A Guerra dos Tronos', 'ano': '1996', 'autor': 'George R.R. Martin', 'genero': 'Fantasia'},
+            {'Codigo do Livro': '0078', 'titulo': 'Orgulho e Preconceito', 'ano': '1813', 'autor': 'Jane Austen', 'genero': 'Romance Clássico'},
+            {'Codigo do Livro': '0234', 'titulo': 'O Senhor dos Anéis: A Sociedade do Anel', 'ano': '1954', 'autor': 'J.R.R. Tolkien', 'genero': 'Fantasia'},
+            {'Codigo do Livro': '0321', 'titulo': 'Cem Anos de Solidão', 'ano': '1967', 'autor': 'Gabriel García Márquez', 'genero': 'Mágico'},
+            {'Codigo do Livro': '0567', 'titulo': 'O Pequeno Príncipe', 'ano': '1943', 'autor': 'Antoine de Saint-Exupéry', 'genero': 'Literatura'},
+            {'Codigo do Livro': '0809', 'titulo': 'A Revolução dos Bichos', 'ano': '1945', 'autor': 'George Orwell', 'genero': 'Ficção'},
+            {'Codigo do Livro': '1098', 'titulo': 'O Alquimista', 'ano': '1988', 'autor': 'Paulo Coelho', 'genero': 'Ficção'},
+            {'Codigo do Livro': '1543', 'titulo': 'O Retrato de Dorian Gray', 'ano': '1890', 'autor': 'Oscar Wilde', 'genero': 'Ficção'},
+            {'Codigo do Livro': '2015', 'titulo': '1984', 'ano': '1949', 'autor': 'George Orwell', 'genero': 'Ficção'},
             {'Codigo do Livro': '0001', 'titulo': 'Dom Quixote', 'ano': '1605', 'autor': 'Miguel de Cervantes', 'genero': 'Romance'},
             {'Codigo do Livro': '0032', 'titulo': 'A Guerra dos Tronos', 'ano': '1996', 'autor': 'George R.R. Martin', 'genero': 'Fantasia'}
         ]
+#-------------------------------------------------------------------------------------------------------------------------------
 
     def cadastrar_cliente(self, nome, idade):
         cliente = {
@@ -28,8 +42,9 @@ class Biblioteca():
             "Idade" : idade,
         }
         self.banco_dados_usuarios.append(cliente)
+        return
         
-
+#-------------------------------------------------------------------------------------------------------------------------------
 
     def cadastrar_livro(self, codigo, titulo, ano, autor, genero):
         livro = {
@@ -41,47 +56,57 @@ class Biblioteca():
         }
         self._livros_cadastrados.append(livro)
         self._estoque.append(livro)
+        return
 
-        #e uma lista entao tem index
+#-------------------------------------------------------------------------------------------------------------------------------
+
     def emprestar_livro(self, titulo, nome):
-        for usuarios in self.banco_dados_usuarios:
+        usuario_encontrado = False
+        livro_cadastrado = False
+
+        for usario in self.banco_dados_usuarios:
+            if usario["Nome"] == nome:
+                usuario_encontrado = True
+
             
-            if (usuarios["Nome"] == nome):
-                for livro_cadastrado in self._livros_cadastrados:
+        if usuario_encontrado == True:
+            for livro_pesquisado in self._livros_cadastrados:
+                if livro_pesquisado["titulo"] == titulo:
+                    livro_cadastrado = True
+                    
+            
+            if livro_cadastrado == True:
+                for livro_disponivel in self._estoque:
+                    if livro_disponivel["titulo"] == titulo:
+                        emprestimo = {
+                            "Livro" : livro_disponivel,
+                            "Usuario" : nome
+                        }
+                        self.livro_emprestado.append(emprestimo)
+                        self._estoque.remove(livro_disponivel)
+                        print(f"Pronto! O livro: {titulo} foi emprestado para {nome}")
 
-                    if livro_cadastrado['titulo'] == titulo: 
-                        for livro_estoque in self._estoque:
-
-                            if livro_estoque['titulo'] == titulo:  
-                                emprestimo = {
-                                    "Livro" : livro_estoque,
-                                    "Usuario" : nome
-                                }
-                                self.livro_emprestado.append(emprestimo)
-                                self._estoque.remove(livro_estoque)
-                                print(f"O livro: {titulo} foi emprestado para {nome}")
-                                return
-
-                            elif livro_estoque['titulo'] not in self._estoque:
-                                print(f"O livro {titulo} ja foi emprestado")
-                            else:
-                                print(f"O livro {titulo} nao esta disponnivel no momento")
-                    else:
-                        print(f"O livro: {titulo} nao esta cadastrado no nosso banco de dados")
             else:
-                print("Cliente nao cadastrado")
+                print(f"O livro: {titulo} não está no nosso sistema.")
+        else: 
+            print(f"O usuario {nome} não esta cadastrado no nosso sistema.")
+        
+#-------------------------------------------------------------------------------------------------------------------------------
 
     def devolver_livro(self, codigo):
         for livro in self.livro_emprestado:
             if (livro['Livro']['Codigo do Livro'] == codigo):
                 self.livro_emprestado.remove(livro)
                 self._estoque.append(livro["Livro"])
-    
+                return
+#-------------------------------------------------------------------------------------------------------------------------------
+
     def pesquisar_livros(self, categoria, conteudo):
         for livro in self._livros_cadastrados:
             if livro[categoria] == conteudo:
                 print("Livro: {}".format(livro["titulo"]))
-            
+        
+#-------------------------------------------------------------------------------------------------------------------------------
             
     
 
@@ -89,7 +114,8 @@ gerente = Biblioteca('Juan')
 gerente.cadastrar_cliente("Pedro", "18")
 
 gerente.cadastrar_livro("1518","Frankenstein", "1998", "Mary Shelley", "Suspense")
-print(gerente.emprestar_livro("Frankenstein", "Pedro"))
+gerente.cadastrar_livro("1552", "Juan", "2005", "Juan", "Romance")
+print(gerente.emprestar_livro("Juan", "Pedro"))
 print(gerente.devolver_livro("1518"))
-print(gerente.pesquisar_livros('genero',"Suspense"))
+print(gerente.pesquisar_livros('genero',"Ficção"))
 
